8e02c4bb5337ac0e1bab6293385ff42a2b881768
devfadyhany <dev.fadyhany@gmail.com>
2026-04-19 18:45:57 +0200

    revert back to BrowserRouter instead of HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { BrowserRouter, HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/Portfolio">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
